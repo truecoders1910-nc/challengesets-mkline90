e7e994627234dee639911ccfa9a8e97fcab94442
Michael Kline <mikeymaederk@gmail.com>
2019-12-06 21:08:29 -0600

    Divide solution

--- ChallengeSets/ChallengeSet03.cs
+++ ChallengeSets/ChallengeSet03.cs
@@ -53,12 +53,16 @@ namespace ChallengeSets
         public char GetLastLetterOfString(string val)
         {
 
-            throw new NotImplementedException();
+            return val.Last();
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            return dividend / divisor;
         }
 
         public int LastMinusFirst(int[] nums)
