8128414e099082a5fae174f473e57088fe6ef88d
Michael Kline <mikeymaederk@gmail.com>
2020-02-14 19:36:16 -0600

    all solutions

--- ChallengeSets/ChallengeSet07.cs
+++ ChallengeSets/ChallengeSet07.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengeSets
 {
@@ -7,18 +8,39 @@ namespace ChallengeSets
     {
         public int CountOfBusinessesWithNegativeNetProfit(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            if (businesses == null)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+            foreach (Business business in businesses)
+            {
+                if (business.TotalRevenue < business.TotalExpenses)
+                {
+                    sum++;
+                }
+            }
+            return sum;
+           
         }
 
         public string GetCommaSeparatedListOfProfitableBusinesses(List<Business> businesses)
         {
-            throw new NotImplementedException();
+            var list = businesses.Where(x => x.TotalRevenue - x.TotalExpenses > 0).Select(x => x.Name);
+
+            return string.Join(",", list);
         }
 
         public string GetNameOfHighestParentCompany(Business business)
         {
             // If there is Company A, whose parent is Company B, whose parent is Company C, then given Company A return Company C
-            throw new NotImplementedException();
+            if (business.ParentCompany == null)
+            {
+                return business.Name;
+            }
+            business.Name = GetNameOfHighestParentCompany(business.ParentCompany);
+            return business.Name;
         }
 
         public enum TicTacToeResult { X, O, Draw }
@@ -29,7 +51,24 @@ namespace ChallengeSets
 
         public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+            int count = 0;
+            foreach (int[] item in numbers)
+            {
+                if (item.Contains(targetNumber))
+                {
+                    count++;
+                }
+            }
+            if (count == numbers.Length)
+            {
+                return true;
+            }
+            return false;
+
         }
     }
 }
